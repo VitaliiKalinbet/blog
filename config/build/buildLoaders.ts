2fb48ddfe6e5c343a9f41f3eecd66d9905ba3dc1
VitaliiKalinbet <bosssvit@gmail.com>
2023-12-12 13:00:53 +0200

    04 added react and css loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,19 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const cssLoader = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        };
+
+    // Якщо не використовуємо typescript то потрібен би був babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -11,5 +24,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     // для цього виносомо окремі лоадери в змінні вище щоб тут зручно бачити порядок лоадерів
     return [
         typescriptLoader,
+        cssLoader,
     ];
 }
