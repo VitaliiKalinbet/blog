d22e1ba018eabab7692e6ce9a6fc5873a81be615
VitaliiKalinbet <bosssvit@gmail.com>
2023-12-14 14:55:25 +0200

    07 added theme, theme context and hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { Suspense } from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+        <div className={`app ${theme}`}>
+            <div>
+                <button onClick={toggleTheme}>Toggle theme</button>
+            </div>
+            <Link to={'/'}>Головна</Link>
+            <Link to={'/about'}>Про сайт</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path={'/about'} element={<AboutPageAsync />} />
