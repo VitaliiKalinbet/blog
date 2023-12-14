138ef7681cd1b4dbf7f63e144e517daadde9d6d0
VitaliiKalinbet <bosssvit@gmail.com>
2023-12-14 16:53:07 +0200

    10 configured project architectury

---
+++ src/pages/AboutPage/index.ts
@@ -0,0 +1,2 @@
+// в index.ts файлах шляхи мають бути відносними а не абсолютними щоб модна було легко переносити кудись ці файли
+export {AboutPageAsync as AboutPage} from './ui/AboutPage.async';
---
+++ src/pages/MainPage/index.ts
@@ -0,0 +1,4 @@
+// в index.ts файлах шляхи мають бути відносними а не абсолютними щоб модна було легко переносити кудись ці файли
+import { MainPageAsync } from './ui/MainPage.async';
+
+export { MainPageAsync as MainPage };
