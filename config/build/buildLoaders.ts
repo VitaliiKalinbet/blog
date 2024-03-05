017d52add3920377bdf77e666614fea6a38dc11f
VitaliiKalinbet <bosssvit@gmail.com>
2024-03-05 15:50:22 +0200

    17 added babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -34,6 +34,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                        [
+                        "i18next-extract",
+                            {
+                                locales: ["uk", "en"],
+                                keyAsDefaultValue: true
+                            }
+                        ]
+                ]
+            }
+        }
+    };
+
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
@@ -48,6 +68,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
