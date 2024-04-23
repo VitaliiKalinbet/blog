1765846606836f9a0f201c596c63e033833bcab9
VitaliiKalinbet <bosssvit@gmail.com>
2024-04-23 16:45:33 +0300

    36 add tests for login feature

---
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginUsername } from 'features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername';
+
+describe('getLoginUsername.test', () => {
+    test('should return mock username', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                username: 'asdf',
+            },
+        };
+        expect(getLoginUsername(state as StateSchema)).toEqual('asdf');
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginUsername(state as StateSchema)).toEqual('');
+    });
+});
