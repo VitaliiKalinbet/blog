9541bf335d90f1b4a65128de7505e37d365e0344
VitaliiKalinbet <bosssvit@gmail.com>
2024-05-01 13:07:37 +0300

    48 add Articles and Article details pages

--- src/widgets/SideBar/model/item.ts
+++ src/widgets/SideBar/model/item.ts
@@ -3,6 +3,7 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
+import ArticleIcon from 'shared/assets/icons/article-20-20.svg';
 
 export interface SideBarItemType {
     path: string;
@@ -29,6 +30,13 @@ export const SideBarItemsList: SideBarItemType[] = [
         text: 'Profile page',
         // @ts-ignore
         Icon: ProfileIcon,
+        authOnly: true,
+    },
+    {
+        path: RoutePath.articles,
+        text: 'Article page',
+        // @ts-ignore
+        Icon: ArticleIcon,
         authOnly: true,
     },
 ];
