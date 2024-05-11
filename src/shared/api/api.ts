2c686a37e9b22059f8baf2274faec268b26d21a2
VitaliiKalinbet <bosssvit@gmail.com>
2024-05-11 16:15:49 +0300

    improve netlify.toml 4th

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -7,7 +7,7 @@ export const $api = axios.create({
 
 $api.interceptors.request.use((config) => {
     if (config.headers) {
-        config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || 'authorizationtoken';
+        config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
     }
 
     return config;
